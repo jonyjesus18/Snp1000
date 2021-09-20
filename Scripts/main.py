623cb7f91c8d550aef12b514020ce36f9f9b1201
Joao Jesus <joaojesusbath@users.noreply.github.com>
2021-09-20 12:15:04 +0100

    2

--- Scripts/main.py
+++ Scripts/main.py
@@ -58,8 +58,8 @@ def openFinViz():
 
 
 def finVizEngine(input,output):
-    import chromedriver_autoinstaller
-    chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
+    #import chromedriver_autoinstaller
+    #chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
                                       # and if it doesn't exist, download it automatically,
                                       # then add chromedriver to path
 
